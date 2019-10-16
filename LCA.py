4aa2946fb1f8a95a5af1299aaca22ac785ed9b6e
Kathleen Garrity <garrityk@bc.edu>
2019-10-17 00:18:47 +0100

    edits to lca alg and test code for dealing with graphs

--- LCA.py
+++ LCA.py
@@ -3,130 +3,50 @@
 
 # originally found on https://www.techiedelight.com/find-lowest-common-ancestor-lca-two-nodes-binary-tree/
 
+# from binarytree import Node, tree, bst, heap, build
+import binarytree
 import networkx as nx
-
 class Node:
 
 	# A utility function to create a new node
-	def __init__(self, key = None):
-		self.data = key
+	def __init__(self, value):
+		self.value = value
 		self.left = None
 		self.right = None
-
-		#for creating a three-node
-		self.center = None
 
 	def __repr__(self):
 		return str(self.data)
 
+def LCA_total(structure, n1, n2):
+	# if the structure is a tree do one thing
+	if isinstance(structure, Node) or isinstance(structure, binarytree.Node):
+		find_lca(structure, n1, n2)
 
-count = 0
+	elif:
+		# isinstance(structure, Graph):
+		# find_lca_graph
 
-def repeats(root, n1, count):
-	if root.data == n1:
-		count += 1
-	if root.left:
-		repeats(root.left, n1, count)
-	if root.right:
-		repeats(root.right, n1, count)
+def find_lca_graph(graph, n1, n2):
+	# make sure the n1 and n2 are graph items
+	
 
-	if count > 1:
-		return True
-	# so if there are more than 1 nodes with the same value, this function will return true
-	# now we want to run this on each node before we find the lca and if it is true, we need to find the LCA of each one
-	# should try to tag each repeat as we find it
-
-
-
-
-# This function returns pointer to LCA of two given values n1 and n2
-# This function also verify that n1 and n2 are present in Binary Tree
-
-
-def findLCA(root, n1, n2):
-	# iterations = 1
-	#
-	# icrement = iterations+1
-	# if icrement ==1:
-	# 	if str(type(root)) != "class '__main__.Node'":
-	# 		return("Invalid Input")
-
-	if not root:
-		# print("this is what not root does: ")
-		return root
-
-	# findLCA.n1 and findLCA.n2 are boolean variables to verify nodes exists in tree.
-	# Current node Match n1
-	if root.data == n1:
-		findLCA.n1 = True
-
-	# Current node Match n2
-	if root.data == n2:
-		findLCA.n2 = True
-
-	# Both have been Matched no need to go any further.
-	# This condition is added for scenario where both nodes are found no need to
-	# further down the tree.
-	if findLCA.n1 and findLCA.n2:
-		return root
-
-	# if lca_left not null atleast one of the nodes is present in left sub tree
-	lca_left = findLCA(root.left, n1, n2)
-
-	# Both have been Matched no need to go any further
-	if findLCA.n1 and findLCA.n2:
-		return lca_left
-
-	# if lca_right not null at least one of the nodes is present in right sub tree
-	lca_right = findLCA(root.right, n1, n2)
-
-	# This condition is added for scenario where one of the requested node is LCA
-	# Here we will override the child result received from parent node
-	if root.data == n1 or root.data == n2:
-		return root
-
-	# if one node is present in left subtree and other in right subtree.
-	# this will be your lowest common ancestor
-	if lca_left and lca_right:
-		return root
-
-	if repeats(root, n1, 0):
-		print("This node value ", n1, "occurs more than once.")
-	if repeats(root, n2, 0):
-		print("This node value ", n2, "occurs more than once.")
+def lca(root, a, b):
+    if not root: return None
+    if root.value == a.value or root.value == b.value: return root
+    left = lca(root.left, a, b)
+    right = lca(root.right, a, b)
+    if left and right:
+        # a & b are on both sides
+        return root
+    else:
+        # EITHER a/b is on one side
+        # OR a/b is not in L&R subtrees
+        return left if left else right
 
 
-
-
-	# if there are two nodes with the same value in the tree
-	# how do we scan the tree to see if the node appears twice? - should do this at the beginning
-
-	# return if any of the subtree returned a result
-	return lca_right if lca_right else lca_left
-
-# This Methods finds Lca and also verify both node are present in tree
 def find_lca(root, n1, n2):
-	findLCA.n1 = False
-	findLCA.n2 = False
-	lca = findLCA(root, n1, n2)
-	if findLCA.n1 and findLCA.n2:
-		return lca
-	# elif (lca == "Invalid Input"):
-	# 	return lca
-	else:
-		return Node(None)
+	if not isinstance(root, Node) or not isinstance(n1, Node) or not isinstance(n2, Node):
+		if not isinstance(root, binarytree.Node) or not isinstance(n1, binarytree.Node) or not isinstance(n2, binarytree.Node):
+			return False
 
-# functions for more easliy creating a very big Tree
-
-def add_right_Nodes(root, values, i):
-	while i < len(values):
-		root.right = Node(values[i])
-		i += 1
-		add_right_Nodes(root.right, values, i)
-
-
-def add_left_Nodes(root, values, i):
-	while i < len(values):
-		root.left = Node(values[i])
-		i += 1
-		add_right_Nodes(root.left, values, i)
+	return lca(root, n1, n2)
